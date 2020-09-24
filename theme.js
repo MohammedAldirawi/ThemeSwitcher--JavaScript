cf441d360878dd8c62ea4bb5358013534ef00ee8
mohtareq <mtareq571@gmail.com>
2020-09-24 13:51:25 +0300

    seconed change

--- theme.js
+++ theme.js
@@ -5,7 +5,7 @@ function setTheme(themeName) {
 }
 
 // function to toggle between light and dark theme
-function toggleTheme() {
+function changeTheme() {
     if (localStorage.getItem('theme') === 'theme-dark') {
         setTheme('theme-light');
     } else {
